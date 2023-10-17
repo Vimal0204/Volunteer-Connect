e4172f4c1dd475f7ce726f5c7327d635ee19c5fa
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-10-18 00:52:15 +0530

    update signup.dart

---
+++ signup.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fresh_farm/widget/customer_drawer.dart';
+import 'package:fresh_farm/widget/seller_list.dart';
+
+class CustomerHomeScreen extends StatelessWidget {
+  const CustomerHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    void onSelectOption(String identifier) {
+      Navigator.of(context).pop();
+    }
+
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Customers'),
+      ),
+      drawer: MainDrawer(
+        onSelectOption: onSelectOption,
+        customerName: FirebaseAuth.instance.currentUser?.email,
+      ),
+      body: const SellerList(),
+    );
+  }
+}
